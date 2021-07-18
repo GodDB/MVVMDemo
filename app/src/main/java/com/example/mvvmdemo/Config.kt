20833c39cccb45f1581980cbe542b3eff7bb626f
GodDB <saqwzx002@naver.com>
2021-07-18 13:06:58 +0900

    add api secure

--- app/src/main/java/com/example/mvvmdemo/Config.kt
+++ app/src/main/java/com/example/mvvmdemo/Config.kt
@@ -4,6 +4,4 @@ object Config {
 
     const val BASE_URL = "https://api.themoviedb.org"
     const val IMAGE_URL = "https://image.tmdb.org/t/p/original"
-
-    const val API_KEY = "fe2614d7c3080f0484d31e19c99a04ee"
 }
--- app/src/main/java/com/example/mvvmdemo/network/interceptor/DefaultParamsInterceptor.kt
+++ app/src/main/java/com/example/mvvmdemo/network/interceptor/DefaultParamsInterceptor.kt
@@ -1,5 +1,6 @@
 package com.example.mvvmdemo.network.interceptor
 
+import com.example.mvvmdemo.BuildConfig
 import com.example.mvvmdemo.Config
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -9,7 +10,7 @@ class DefaultParamsInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
         val newHttpUrl = original.url.newBuilder()
-            .addQueryParameter("api_key", Config.API_KEY)
+            .addQueryParameter("api_key", BuildConfig.API_KEY)
             .build()
 
         val newRequestBuilder = original.newBuilder()
